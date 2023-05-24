2a1612268bcb39db63b90ad3297833e397856c17
Diego Araya <diegoaraya641@gmail.com>
2023-05-24 10:07:55 -0400

    imagen

--- clienteSoapLibreria/core/producto.py
+++ clienteSoapLibreria/core/producto.py
@@ -5,14 +5,16 @@ class Producto:
     stock=''
     fecha_entrega=''
     categoria=''
+    imagen = ''
     
-    def __init__(self,id_producto,nombre,precio_bruto,stock,fecha_entrega,categoria):
+    def __init__(self,id_producto,nombre,precio_bruto,stock,fecha_entrega,categoria,imagen):
         self.id_producto = id_producto
         self.nombre = nombre
         self.precio = precio_bruto
         self.stock = stock
         self.fecha_entrega = fecha_entrega
         self.categoria = categoria
+        self.imagen = imagen
 
     
     def libroArr(self):
@@ -22,7 +24,8 @@ class Producto:
             'precio_bruto': self.precio_bruto,
             'stock': self.stock,
             'fecha_entrega':self.fecha_entrega,
-            'categoria':self.categoria
+            'categoria':self.categoria,
+            'imagen' : self.imagen
         }
     
     def __str__(self):
