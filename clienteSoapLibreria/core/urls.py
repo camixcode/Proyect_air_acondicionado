b1181ca5c593e07e6ca32008c24fed37c6648155
AlexD30 <101906399+AlexD30@users.noreply.github.com>
2023-06-01 16:33:48 -0400

    Inicio

--- clienteSoapLibreria/core/urls.py
+++ clienteSoapLibreria/core/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import agregar_producto, eliminar_producto, historialVenta, home,buscarLibro,actualizarLibro,buscarProducto,actualizarStockProducto, inicio, limpiar_carrito, productos, registro , adminServicios, restar_producto, servicios
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('',productos, name="productos"),
+    path('home', home, name='home'),
     path('post/ajax/buscar/',buscarLibro, name='post_buscar'),
     path('post/ajax/actualizar/',actualizarLibro, name='post_actualizar'),
     path('post/ajax/buscar_producto/',buscarProducto, name='post_buscar_producto'),
     path('post/ajax/actualizar_producto/',actualizarStockProducto, name='post_actualizar_producto'),
     path('registro',registro, name="registro"),
-    path('productos',productos, name="productos"),
+    
     path('historialVenta',historialVenta, name="historialVenta"),
     path('inicio',inicio, name="inicio"),
     path('adminServicios',adminServicios, name="adminServicios"),
