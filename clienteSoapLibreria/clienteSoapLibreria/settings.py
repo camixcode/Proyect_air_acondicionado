698ac3743ea1c9e3baaa07ffc7032ff328035ca0
Diego Araya <diegoaraya641@gmail.com>
2023-06-29 22:43:51 -0400

    c

--- clienteSoapLibreria/clienteSoapLibreria/settings.py
+++ clienteSoapLibreria/clienteSoapLibreria/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 es_formats.DATETIME_FORMAT = "d/m/Y H:i:s"
 
 
